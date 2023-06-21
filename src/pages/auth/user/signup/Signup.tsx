db46ab83fcbd198878d26439b517062dbd7dc5d3
ianK-code <kian99564@gmail.com>
2023-06-21 16:27:46 +0300

    Completed with auth in user side

--- src/pages/auth/user/signup/Signup.tsx
+++ src/pages/auth/user/signup/Signup.tsx
@@ -8,18 +8,20 @@ import {
 } from "@mantine/core";
 import { useForm, yupResolver } from "@mantine/form";
 import {
-  LoginInput,
-  loginValidator,
+  SignupInput,
+  signupValidator,
 } from "../../../../utils/validators/auth.validators";
 
 export function Signup() {
-  const form = useForm<LoginInput>({
+  const form = useForm<SignupInput>({
     initialValues: {
+      name: "",
       email: "",
       password: "",
+      password_confirmation: "",
     },
 
-    validate: yupResolver(loginValidator),
+    validate: yupResolver(signupValidator),
   });
 
   return (
@@ -28,15 +30,30 @@ export function Signup() {
       <form onSubmit={form.onSubmit((values) => console.log(values))}>
         <TextInput
           withAsterisk
+          label="Full Name"
+          placeholder="Enter your full name"
+          {...form.getInputProps("name")}
+        />
+
+        <TextInput
+          withAsterisk
           label="Email"
           placeholder="your@email.com"
           {...form.getInputProps("email")}
         />
+
         <PasswordInput
           withAsterisk
           label="Password"
           placeholder="Enter your password"
           {...form.getInputProps("password")}
+        />
+
+        <PasswordInput
+          withAsterisk
+          label="Confirm Password"
+          placeholder="Enter your confirmation password"
+          {...form.getInputProps("password_confirmation")}
         />
 
         <Group position="left" mt="md">
--- src/utils/validators/auth.validators.ts
+++ src/utils/validators/auth.validators.ts
@@ -6,3 +6,23 @@ export const loginValidator = yup.object({
 });
 
 export type LoginInput = yup.InferType<typeof loginValidator>;
+
+export const signupValidator = yup.object({
+  name: yup
+    .string()
+    .min(4, "Name must be at least 4 characters")
+    .max(50, "Name must be less than 50 characters")
+    .required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(4).required(),
+  password_confirmation: yup
+    .string()
+    .min(4, "Password confirmation must be at least 4 characters")
+    .required("Password confirmation is required")
+    .oneOf([
+      yup.ref("password"),
+      "Password confirmation does not match with your password",
+    ]),
+});
+
+export type SignupInput = yup.InferType<typeof signupValidator>;
