a48ca57c82857c7a640f5948784e8be51e32d7f9
ianK-code <kian99564@gmail.com>
2023-06-28 14:10:05 +0300

    All meals are available on the landing page

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,12 +1,13 @@
 import "./landing.css";
 // import Footer from "../../components/footer/Footer";
 import Nav from "../../components/navbar/Nav";
+import AllMeals from "../other/AllMeals";
 
 function Landing() {
   return (
     <>
       <Nav />
-      <div></div>
+      <AllMeals />
       {/* <Footer /> */}
     </>
   );
