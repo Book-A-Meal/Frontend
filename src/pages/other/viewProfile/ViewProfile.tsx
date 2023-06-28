7849a013609d4eecfab1244335ab7dcf89ce5262
ianK-code <kian99564@gmail.com>
2023-06-28 14:03:49 +0300

    Wrote an interface code for admin profile

--- src/pages/other/viewProfile/ViewProfile.tsx
+++ src/pages/other/viewProfile/ViewProfile.tsx
@@ -1,24 +1,44 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Avatar } from "@mantine/core";
 import axios from "axios";
 
+interface UserProfile {
+  admin: {
+    id: number;
+    name: string;
+    email: string;
+    IsAdmin: boolean;
+  };
+  image: string;
+  meals: [];
+}
+
 export default function ViewProfile() {
-    const {id} = useParams()
-    console.log(`This is the id: ${id}`)
-    const [data, setData] = useState<any>()
-    useEffect(() => {
-      axios
-        .get(`http://127.0.0.1:3000/admins/${id}`)
-        .then((res) => {
-          console.log(res.data.data.admin);
-          setData(res.data.data.admin);
-        })
-        .catch((error) => {
-          // Handle error state or display error message
-          console.error(error);
-        });
-    }, []);
+  const { id } = useParams();
+  const [data, setData] = useState<UserProfile>();
+  useEffect(() => {
+    axios
+      .get(`http://127.0.0.1:3000/admins/${id}`)
+      .then((res) => {
+        console.log(res.data.data);
+        setData(res.data.data);
+      })
+      .catch((error) => {
+        // Handle error state or display error message
+        console.error(error);
+      });
+  }, []);
   return (
-    <div>ViewProfile</div>
-  )
+    <>
+      {data && (
+        <div>
+          <span>{data.admin.name}</span>
+          <div>
+            <Avatar size={150} src={data.image} />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
