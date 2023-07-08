1771a26016fdf6969712110b3c89faedf3779abe
ianK-code <kian99564@gmail.com>
2023-07-08 23:26:38 +0300

    Prepairing EditMeal page

--- src/pages/dashboard/pages/EditMeal.tsx
+++ src/pages/dashboard/pages/EditMeal.tsx
@@ -1,7 +1,10 @@
+import { useForm } from "@mantine/form";
+import { notifications } from "@mantine/notifications";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 function EditMeal() {
-  return (
-    <div>EditMeal</div>
-  )
+  return <div>EditMeal</div>;
 }
 
-export default EditMeal
+export default EditMeal;
